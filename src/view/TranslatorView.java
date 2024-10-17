440d10c4e16f0cdcdfaa668fd24f177c522e1f80
寺澤徳隆 <mosqey1889@gmail.com>
2024-10-17 15:18:48 +0900

    コンソールビューの作成

--- src/view/TranslatorView.java
+++ src/view/TranslatorView.java
@@ -1,4 +1,21 @@
 package view;
 
+import java.util.Scanner;
+
+// ユーザとのやり取りを管理するクラス
 public class TranslatorView {
+    // 入力用スキャナーの準備
+    private Scanner scanner = new Scanner(System.in);
+
+    // ユーザからの入力を取得するメソッド
+    public String getInput() {
+        System.out.println("翻訳したいテキスト(英語または日本語)を入力してください");
+        return scanner.nextLine();
+    }
+
+    // 翻訳結果を表示するメソッド
+    public void displayTranslation(String translatedText) {
+        System.out.println("翻訳結果:　" + translatedText);
+    }
+
 }
