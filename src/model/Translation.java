d6d4e7a0a3f7c9513037b44dde743f61062afb74
寺澤徳隆 <mosqey1889@gmail.com>
2024-10-17 15:04:14 +0900

    翻訳モデルの作成

--- src/model/Translation.java
+++ src/model/Translation.java
@@ -1,4 +1,25 @@
 package model;
 
+// 翻訳データを表現するクラス
 public class Translation {
+    // 原文
+    private String originalText;
+    // 翻訳結果
+    private String translatedText;
+
+    // コンストラクタ
+    public Translation(String originalText, String translatedText) {
+        this.originalText = originalText;
+        this.translatedText = translatedText;
+    }
+
+    // 原文を取得するメソッド
+    public String getOriginalText() {
+        return originalText;
+    }
+
+    // 翻訳結果を取得するメソッド
+    public String getTranslatedText() {
+        return translatedText;
+    }
 }
